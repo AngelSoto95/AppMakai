c2013d83311227da89fda5e1549d7edd1f9692de
Angel Soto <angelsotoo9525@gmail.com>
2024-01-18 22:37:36 -0500

    Se realizan cambios a la aplicacion en general

--- Velero.java
+++ Velero.java
@@ -6,42 +6,22 @@ import Makai.Embarcacion;
 public class Velero extends Embarcacion {
 
     private int cantidadMastiles;
-    private int cantidadVelas;
 
-    public Velero(Capitan capitan, double precioBase, int valorAdicional, int fechaFabricacion, double montoAlquiler, int cantidadMastiles, int cantidadVelas) {
-        super(capitan, precioBase, valorAdicional, fechaFabricacion, montoAlquiler);
+    public Velero(Capitan capitan, int precioBase, int valorAdicional, int fechaFabricacion, int esloraMetros, int cantidadMastiles) {
+        super(capitan, precioBase, valorAdicional, fechaFabricacion, esloraMetros);
         this.cantidadMastiles = cantidadMastiles;
-        this.cantidadVelas = cantidadVelas;
     }
 
-    public int getCantidadMastiles() {
-
-        return cantidadMastiles;
-    }
-
-    public int getCantidadVelas() {
-
-        return cantidadVelas;
-    }
-
-    @Override
-    public double calcularAlquiler() {
-        double montoAlquiler = getPrecioBase() + getValorAdicional();
-
-        if (getFechaFabricacion() > 2020) {
-            return montoAlquiler;
-        }
-
-        setMontoAlquiler(montoAlquiler);
-        //System.out.println("El monto del alquiler es: " + getMontoAlquiler());
-
-        return getMontoAlquiler();
-    }
     public void mostrarInfo(){
         System.out.println("La informacion a mostrar es: " );
-        System.out.println("precio base: " + getPrecioBase() + " Monto alquiler: " + getMontoAlquiler() + " Fecha fabricacion : " + getFechaFabricacion() + " Cantidad mastiles: " + getCantidadMastiles() + " Cantidad de velas: " + getCantidadVelas() );
+        System.out.println("precio base: " + getPrecioBase() + " Monto alquiler: " + " Fecha fabricacion : " + getFechaFabricacion());
     }
-    public boolean definirTamaño() {
-        return cantidadMastiles > 4;
+    public void evaluarTamanio() {
+        if (cantidadMastiles > 4){
+            System.out.println("Su velero es grande");
+        }else{
+            System.out.println("Su velero es pequeño");
+        }
     }
-    }
+
+}
